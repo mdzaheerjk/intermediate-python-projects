7cee1c5a9e031a28f4e4edd913cbe3ec39acbd42
Mohd Zaheeruddin <info.zaheerjk@gmail.com>
2024-12-10 07:57:22 +0530

    Add files via upload

---
+++ zaheer_jk_chat_bot.py
@@ -0,0 +1,15 @@
+from operator import truediv
+
+import google.generativeai as ai
+API_KEY ='AIzaSyBDwbNGbJN5XXz1tksGSL5w9NXyeswraww'
+ai.configure(api_key=API_KEY)
+model = ai.GenerativeModel("gemini-pro")
+chat = model.start_chat()
+chat_over=True
+while chat_over:
+    message = input('You: ')
+    if message.lower() == 'bye':
+        print('Chatbot: Goodbye!')
+        chat_over=False
+    response = chat.send_message(message)
+    print('Chatbot:', response.text)
